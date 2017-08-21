41e61ddb0452b9f50d3c019df5fdf1edf3ccfa70
Grant Ferguson <grant@grantferguson.com>
2017-08-20 21:50:25 -0600

    Added Menu and jquer/Modernizr dependencies

--- Personal-Site/ClientApp/app/app.module.shared.ts
+++ Personal-Site/ClientApp/app/app.module.shared.ts
@@ -5,31 +5,33 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { NavMenuComponent } from './components/shared/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { NavHeaderComponent } from './components/shared/navheader/navheader.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
-    ],
-    imports: [
-        CommonModule,
-        HttpModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+	declarations: [
+		AppComponent,
+		NavMenuComponent,
+		CounterComponent,
+		FetchDataComponent,
+		HomeComponent,
+		NavHeaderComponent
+	],
+	imports: [
+		CommonModule,
+		HttpModule,
+		FormsModule,
+		RouterModule.forRoot([
+			{ path: '', redirectTo: 'home', pathMatch: 'full' },
+			{ path: 'home', component: HomeComponent },
+			{ path: 'counter', component: CounterComponent },
+			{ path: 'fetch-data', component: FetchDataComponent },
+			{ path: '**', redirectTo: 'home' }
+		])
+	]
 })
 export class AppModuleShared {
 }
---
+++ Personal-Site/ClientApp/app/components/shared/navheader/navheader.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'nav-header',
+    templateUrl: './navheader.component.html',
+	 styleUrls: ['./navheader.component.css'],
+	 encapsulation: ViewEncapsulation.None
+})
+export class NavHeaderComponent {
+}
